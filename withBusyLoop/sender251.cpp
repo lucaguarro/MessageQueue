fb32107c511f8a162fe19ad6d6a7f7b950f2d383
Luca Guarro <luca.guarro@gmail.com>
2018-03-17 16:32:23 -0700

    program written using a busy loop and without a busy loop

--- withBusyLoop/sender251.cpp
+++ withBusyLoop/sender251.cpp
@@ -17,8 +17,11 @@ using namespace std;
 	This sender terminates on a 'kill' command from a separate terminal.
 */
 int main() {
+	//Used to get information about the queue
 	msqid_ds qInfo;
 	cout << "Sender 251 starting..." << endl;
+
+	//create the message queue
 	int qid = msgget(ftok(".",'u'), IPC_CREAT|0600);
 	cout << "Pid of sender251: " << getpid() << endl;
 	// declare my message buffer
@@ -33,26 +36,32 @@ int main() {
 		int message; // mesg content
 	};
 	buf msg;
+
+	/*
+		We need to tell the queue the size of the data passed into the queue
+	*/
 	int size = sizeof(msg)-sizeof(long);
 
 	/*
 		Before we do anything, we need to tell the patch-code what message to send on behalf of sender251
-		to receiverA, when the user types in the kill command.
-		us
+		to receiverA via the get_info function, when the user types in the kill command. Setup so that it sends a message of 0.
 	*/
 	msg.mtype = 1;
 	msg.message = 0;
 	get_info(qid, (struct msgbuf *)&msg, size, 1);
-	srand(time(NULL));
 
+	//seed to generate random numbers
+	srand(time(NULL));
 	int randoInt;
 	
-	//Generate message between 2 and int max. 0 and 1 are reserved to signal termination.
-	
 	while(true){
+		/*
+			Generate message between 3 and int max that is divisibl by the marker. 0, 1, & 2 are reserved to signal termination.
+			marker = 251 for this program.
+		*/
 		do
 		{
-			randoInt = rand() % (RAND_MAX-3) + 3; //Generate message between 3 and int max. 0, 1, & 2 are reserved to signal termination.
+			randoInt = rand() % (RAND_MAX-3) + 3;
 		}while(randoInt%251 != 0);
 		/*
 			There is also a chance that the number can be divided by 997. This would cause issues because 997 requires acknowledgement
@@ -62,16 +71,18 @@ int main() {
 		if(randoInt % 997 == 0){
 			randoInt = randoInt - 251;
 		}
+
+		//	Send message to receiver A
 		msg.message = randoInt;
 		msgsnd(qid, (struct msgbuf *)&msg, size, 0); // HALTS
 
-		//this do-while loop should make sure that the message queue does not get overfilled
+		/*
+			This loop is to ensure that sender251 does not max out the queue. We need to ensure that receiverA can always send an acknowledgment
+			message back to sender997.
+		*/
 		do{
 			msgctl(qid, IPC_STAT, &qInfo);
 		}while((qInfo.msg_qbytes/size)-2 <= qInfo.msg_qnum); //while the number of messages in the queue are greater than or equal the max - 2
-		
-		//cout << "max bytes: " << qInfo.msg_qbytes;
-		//cout << "Num messages on queue: " << qInfo.msg_qnum << endl;
 
 	}
 	exit(0);
